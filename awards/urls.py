140a39be2e7e591deeae635b335e260067552313
saudahabib <saudababs00@gmail.com>
2019-06-03 22:58:12 +0300

    fix bug on posting new projects

--- awards/urls.py
+++ awards/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.welcome,name = 'welcome'),
     url(r'^project/(\d+)/',views.project,name ='project'),
-    url(r'^new/article$', views.new_project, name='new-project'),
+    url(r'^new/article/$', views.new_project, name='new-project'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
