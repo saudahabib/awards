e5bc8a8585931809763d432643761d493746f204
saudahabib <saudababs00@gmail.com>
2019-06-03 18:13:02 +0300

    installed updated version of django reg

--- awarding/urls.py
+++ awarding/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^tinymce/', include('tinymce.urls')),
-
-
 ]
