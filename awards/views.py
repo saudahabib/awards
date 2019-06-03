5f69b50c1d4ea54af68db4c7c114bb75db4a24e6
saudahabib <saudababs00@gmail.com>
2019-06-03 12:46:40 +0300

    successfully display single articles

--- awards/views.py
+++ awards/views.py
@@ -14,6 +14,6 @@ def project(request, project_id):
     try:
         project = Project.objects.get(id=project_id)
         projects = Project.objects.all()
-    except DoesNotExist:
+    except:
         raise Http404()
     return render(request, "single_project.html", {"project":project, "projects":projects})
