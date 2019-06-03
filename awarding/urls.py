4287c5293f8a3fb7164c5e6162d32b5f1e8bdcee
saudahabib <saudababs00@gmail.com>
2019-06-03 15:27:16 +0300

    implemented login/logout functionality

--- awarding/urls.py
+++ awarding/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
-
+from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('awards.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
