4d21722616f9660ea4d8b52da55fd249127a055d
saudahabib <saudababs00@gmail.com>
2019-06-03 09:18:35 +0300

    restructure landing page: restore bootstrap

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from .models import Image, Profile
-from .forms import NewPostForm, NewsProfileForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from .models import Profile
+
+
 # Create your views here.
 '''welcome view to process landing page'''
 def welcome(request):
